a6ad22451901df43c3c156db74879bb4495fcebb
Iv1n <90642026+Iv1n@users.noreply.github.com>
2023-01-31 19:44:27 +0300

    Delete bank/Bank directory

--- bank/Bank/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.bank {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.bank to javafx.fxml;
-    exports com.example.bank;
-}
